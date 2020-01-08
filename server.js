af0c842a6f9579906a1ae6d20c1160a751e476b7
svikone <velichkovlad825@gmail.com>
2020-01-09 00:09:03 +0200

    fix cors

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const multer = require('multer');
 app.use('/file', express.static('file'));
 app.use(multer({dest:__dirname+'/file/uploads/'}).any());
 
-app.use(cors({origin: ["http://localhost:8080","http://localhost:8081"]}));
+app.use(cors('*'));
+
+// app.use(cors({origin: ["http://localhost:8080","http://localhost:8081"]}));
+// app.use(cors({ Access-Control-Allow-Origin:
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use (bodyParser.json ({
 	extended: true,
