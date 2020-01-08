d9a069647fb4935e21ec16d88fcaab727002af40
svikone <velichkovlad825@gmail.com>
2020-01-09 00:44:27 +0200

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(multer({dest:__dirname+'/file/uploads/'}).any());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', ["http://localhost:8080","http://localhost:8081"]);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
