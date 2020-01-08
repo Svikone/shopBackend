729b6308fcdab835458ce48cc1d1edeeca00e76c
svikone <velichkovlad825@gmail.com>
2020-01-09 00:32:53 +0200

    fix cors

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const multer = require('multer');
 app.use('/file', express.static('file'));
 app.use(multer({dest:__dirname+'/file/uploads/'}).any());
 
-app.use(cors({origin: '*'}))
-// app.use(cors({origin: ["http://localhost:8080","http://localhost:8081"]}));
-// app.use(cors({ Access-Control-Allow-Origin:
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "https://app-shop12.herokuapp.com"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+app.use(cors({origin: ["http://localhost:8080","http://localhost:8081"]}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use (bodyParser.json ({
 	extended: true,
