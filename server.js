3d4ecf4e30005a88d2ebaaf32635f2a797daf961
svikone <velichkovlad825@gmail.com>
2020-01-09 21:05:55 +0200

    fix cors

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const catalogRouter = require("./routes/catalog");
 const orderRouter = require("./routes/order");
 
 let port = process.env.PORT || 9000;
+app.post('/test',(req,res) => {
+	console.log('work')
+	res.send({message: 'work pls'});
+})
 app.get("/",(req,res) => {
 	res.end("<h1>hello world</h1>")
 })
