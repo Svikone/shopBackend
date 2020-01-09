ed96c790ef31eb265d8f79c590f58d2a04987e9b
svikone <velichkovlad825@gmail.com>
2020-01-09 21:14:05 +0200

    fix cors

--- controller/order.js
+++ controller/order.js
@@ -5,42 +5,43 @@ const orderItemModules = require("../model/order_item");
 
 
 exports.getOrder = (req, res) => {
-    const order = {
-        user_name: req.body.user_name,
-        phone: req.body.phone,
-        status_confirm: false,
-        status_payment: false
-    }
-    const products = req.body.catalogs;
+    res.send({message:'hello lox'})
+    // const order = {
+    //     user_name: req.body.user_name,
+    //     phone: req.body.phone,
+    //     status_confirm: false,
+    //     status_payment: false
+    // }
+    // const products = req.body.catalogs;
 
-    orderModules(order).save().then(first => {
-                // console.log(first)
+    // orderModules(order).save().then(first => {
+    //             // console.log(first)
 
-        products.forEach(product => {
-            catalogModules.findById(product.id).then(second => {
-                // console.log(second)
-                const items = {
-                    id: second._id,
-                    order_id: first._id,
-                    price: second.price,
-                    full_price: second.price * product.number,
-                    number: product.number,
-                    name: second.name
+    //     products.forEach(product => {
+    //         catalogModules.findById(product.id).then(second => {
+    //             // console.log(second)
+    //             const items = {
+    //                 id: second._id,
+    //                 order_id: first._id,
+    //                 price: second.price,
+    //                 full_price: second.price * product.number,
+    //                 number: product.number,
+    //                 name: second.name
 
-                }
+    //             }
 
 
-                orderItemModules(items).save().then(third => {
-                    console.log(third)
-                })
-            })
-        })
-        res.sendStatus(200);
+    //             orderItemModules(items).save().then(third => {
+    //                 console.log(third)
+    //             })
+    //         })
+    //     })
+    //     res.sendStatus(200);
 
-    }).catch(err => {
-        res.sendStatus(500)
+    // }).catch(err => {
+    //     res.sendStatus(500)
 
-    })
+    // })
 }
 
 exports.getAllOrder = (req, res) => {
