4dd91acb3bb36f33ba930029b13491dc6d675408
svikone <velichkovlad825@gmail.com>
2020-01-11 01:04:54 +0200

    db

--- db.js
+++ db.js
@@ -1,27 +1,26 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop" ;
 
-mongoose.connect(uri, { 
-	useUnifiedTopology: true
-	}).then(() => {
-	console.log("Database is worked");
-}).catch( (e) =>{
-	console.log("Database disconnect");
-    process.exit(1);
-}) 
-module.exports = mongoose.connection;
-
-// const db = process.env.MONGODB_URL || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop";
-
-// exports.connectDB = async () => {
-//   try {
-//     await mongoose.connect(db, {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true
-//     });
-//     console.log("MongoDB is Connected...");
-//   } catch (err) {
-//     console.error(err.message);
+// mongoose.connect(uri, { 
+// 	useUnifiedTopology: true
+// 	}).then(() => {
+// 	console.log("Database is worked");
+// }).catch( (e) =>{
+// 	console.log("Database disconnect");
 //     process.exit(1);
-//   }
-// };
+// }) 
+// module.exports = mongoose.connection;
+
+
+const MongoClient = require("mongodb").MongoClient;
+ 
+// создаем объект MongoClient и передаем ему строку подключения
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
+mongoClient.connect(function(err, client){
+ 
+    if(err){
+        return console.log(err);
+    }
+    // взаимодействие с базой данных
+    client.close();
+});
