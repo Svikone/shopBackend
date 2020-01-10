68b93507d9f909eab0a489ea41166c36d85edbc0
svikone <velichkovlad825@gmail.com>
2020-01-10 18:53:34 +0200

    rename port

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://vlad:<admin>@cluster0-vzqoj.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop";
 
 mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
 	console.log("Database is worked");
