52790a0bbe974209db378fb1418d5c928afbd957
svikone <velichkovlad825@gmail.com>
2020-01-10 23:35:06 +0200

    db

--- db.js
+++ db.js
@@ -5,6 +5,21 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(
 	console.log("Database is worked");
 }).catch( (e) =>{
 	console.log("Database disconnect");
+    process.exit(1);
 }) 
 module.exports = mongoose.connection;
 
+// const db = process.env.MONGODB_URL || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop";
+
+// exports.connectDB = async () => {
+//   try {
+//     await mongoose.connect(db, {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true
+//     });
+//     console.log("MongoDB is Connected...");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
