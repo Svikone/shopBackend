dee62287ed566f7eddebceca1cdc6a2135025a31
svikone <velichkovlad825@gmail.com>
2020-01-11 00:15:11 +0200

    db

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop" ;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(uri, { useNewUrlParser: true}).then(() => {
 	console.log("Database is worked");
 }).catch( (e) =>{
 	console.log("Database disconnect");
