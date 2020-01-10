ad23089219521ceca034ba336c443bd46fcca84d
svikone <velichkovlad825@gmail.com>
2020-01-10 23:05:48 +0200

    uri change

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop";
+const uri = "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop" || process.env.MONGO_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
 	console.log("Database is worked");
