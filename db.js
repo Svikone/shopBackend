8a5a15eb893fb2cacdf57e50242a3aca15876d43
svikone <velichkovlad825@gmail.com>
2020-01-11 01:28:19 +0200

    db

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-const uri =  "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop?retryWrites=true&w=majority" ;
+const uri = "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop" ;
 
 mongoose.connect(uri, { 
-	useUnifiedTopology: true,
 	useNewUrlParser: true
 	}).then(() => {
 	console.log("Database is worked");
