e6cc1d75779ffdadbdaba2d8a3334b785889d641
svikone <velichkovlad825@gmail.com>
2020-01-11 01:19:34 +0200

    db

--- db.js
+++ db.js
@@ -1,26 +1,14 @@
-// const mongoose = require("mongoose");
-const uri = process.env.MONGO_URI || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop" ;
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI || "mongodb+srv://vlad:admin@cluster0-vzqoj.mongodb.net/shop?retryWrites=true&w=majority" ;
 
-// mongoose.connect(uri, { 
-// 	useUnifiedTopology: true
-// 	}).then(() => {
-// 	console.log("Database is worked");
-// }).catch( (e) =>{
-// 	console.log("Database disconnect");
-//     process.exit(1);
-// }) 
-// module.exports = mongoose.connection;
+mongoose.connect(uri, { 
+	useUnifiedTopology: true
+	}).then(() => {
+	console.log("Database is worked");
+}).catch( (e) =>{
+	console.log("Database disconnect");
+    process.exit(1);
+}) 
+module.exports = mongoose.connection;
 
 
-const MongoClient = require("mongodb").MongoClient;
- 
-// создаем объект MongoClient и передаем ему строку подключения
-const mongoClient = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
-mongoClient.connect(function(err, client){
- 
-    if(err){
-        return console.log(err);
-    }
-    // взаимодействие с базой данных
-    client.close();
-});
