831a7e4a6d595c9bac495732e3973243c8703f52
svikone <velichkovlad825@gmail.com>
2020-01-21 02:32:14 +0200

    fix route

--- routes/marca.js
+++ routes/marca.js
@@ -4,6 +4,7 @@ const Controller = require("../controller/marca");
 const Router = express.Router()
 
 Router.post("/marca/add",  Controller.addMarc);
+Router.post("/get/marcs",  Controller.getMarcs);
 Router.post("/get/marcs/id",  Controller.getMarcsId);
 Router.post("/marca/update",  Controller.updateMarcs);
 
