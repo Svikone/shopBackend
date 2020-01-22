1f5fbfb767ce263587cf8b3bf350b866816ea187
svikone <velichkovlad825@gmail.com>
2020-01-22 03:22:07 +0200

    fix marca remove

--- routes/marca.js
+++ routes/marca.js
@@ -7,7 +7,7 @@ Router.post("/marca/add",  Controller.addMarc);
 Router.post("/get/marcs",  Controller.getMarcs);
 Router.post("/get/marcs/id",  Controller.getMarcsId);
 Router.post("/marca/update",  Controller.updateMarcs);
-Router.post("/remove/id",  Controller.removeMarcsId);
+Router.post("/remove",  Controller.removeMarcsId);
 
 
 module.exports = Router;
